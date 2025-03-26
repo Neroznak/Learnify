d1587910237b96e6d94b220c33018667502141fd
Neroznak <dxnz@bk.ru>
2025-03-26 13:48:21 +0100

    first run

--- apps/api-gateway/webpack.config.js
+++ apps/api-gateway/webpack.config.js
@@ -3,15 +3,14 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../dist/api-gateway'),
+    path: join(__dirname, '../../dist/api-gateway'),
   },
   plugins: [
     new NxAppWebpackPlugin({
       target: 'node',
       compiler: 'tsc',
-      main: './src/main.ts',
+      main: 'apps/api-gateway/src/main.ts',
       tsConfig: './tsconfig.app.json',
-      assets: ["./src/assets"],
       optimization: false,
       outputHashing: 'none',
       generatePackageJson: true,
